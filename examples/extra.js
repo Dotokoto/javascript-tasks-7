976886ea7b7f8b60db3030583e93ba0328df74ac
Natalia Aksenova <dotokoto@gmail.com>
2015-11-23 12:18:59 +0500

    There's something wrong

--- examples/extra.js
+++ examples/extra.js
@@ -5,15 +5,11 @@
 var check = require('../src/check');
 
 check.init();
+
 var person = { name: 'John', age: 20 };
 var numbers = [1, 2, 3];
 var func = function (a, b) {};
 var str = 'some string';
-var emptyStr = '         ';
-var itsNull = null;
-itsNull = check.wrap(itsNull);
-var undef = undefined;
-undef = check.wrap(undef);
 
 console.log(
     person.check.hasKeys(['name', 'age']), // true
@@ -22,12 +18,9 @@ console.log(
     numbers.check.hasKeys(['0', '1', '2']), // true
     numbers.check.hasLength(3), // true
 
-    numbers.check.hasValues([2, 1]), // false
+    numbers.check.hasValues([2, 1]), // true
 
     func.check.hasParamsCount(2), // true
 
-    str.check.hasWordsCount(2), // true
-    emptyStr.check.hasWordsCount(0),
-    itsNull.check.hasKeys(['name']),
-    undef.check.hasKeys(['name'])
+    str.check.hasWordsCount(2) // true
 );
